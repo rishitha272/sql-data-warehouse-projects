318ac41bd2f55b0adeb0ca2253012611e097cbcd
rishitha272 <116900745+rishitha272@users.noreply.github.com>
2025-06-02 21:20:44 +0530

    Update load_bronze.sql

--- scripts/load_bronze.sql
+++ scripts/load_bronze.sql
@@ -15,10 +15,6 @@ For each table:
 /* 
 -- CRM Tables Data Load
 These commands import data from CSV files located in the 'source_crm' folder into the CRM tables:
-- bronze.crm_cust_info: Customer information data.
-- bronze.crm_prd_info: Product information data.
-- bronze.crm_sales_details: Sales transaction details.
-
 The `\copy` command runs client-side and reads files from your local system.Run on PSQL
 The CSV files have a header row and comma-separated fields.
 */
@@ -33,15 +29,9 @@ SELECT * FROM bronze.crm_cust_info;
 SELECT * FROM bronze.crm_prd_info;
 SELECT * FROM bronze.crm_sales_details;
 
-/* 
--- ERP Tables Data Load
-These commands import data from CSV files located in the 'source_erp' folder into the ERP tables:
-- bronze.erp_loc_a101: Location data.
-- bronze.erp_cust_az12: Customer ERP data.
-- bronze.erp_px_cat_g1v2: Product category data.
 
-Again, the CSV files have headers and comma delimiters.
-*/
+-- ERP Tables Data Load
+
 --Run these commands on PSQL
 \copy bronze.erp_loc_a101 FROM 'D:/Downloads/sql-data-warehouse-project/sql-data-warehouse-project/datasets/source_erp/loc_a101.csv' DELIMITER ',' CSV HEADER;
 \copy bronze.erp_cust_az12 FROM 'D:/Downloads/sql-data-warehouse-project/sql-data-warehouse-project/datasets/source_erp/cust_az12.csv' DELIMITER ',' CSV HEADER;
